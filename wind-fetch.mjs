bf67fb6bce9a05bd7b8c0b4283fae4a1d7977522
Amagi:DDmxh <amagiddmxh@gmail.com>
2023-11-19 09:13:01 +0800

    Revert latest changes

--- wind-fetch.mjs
+++ wind-fetch.mjs
@@ -1,34 +1,21 @@
 import fetch from 'node-fetch';
 
-const apiKey = 'e75e1345a65d4ce1bedc9e59fd3012d6'; // Replace with your OpenCage API key
-const city = 'Istanbul';
+const apiKey = '6d0dbab583f5fa7f2329eebc49b3a906'; // Your OpenWeatherMap API key
+const city = 'London'; // The city for which you want weather data
 
-const url = `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(city)}&key=${apiKey}`;
+const url = `http://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(city)}&appid=${apiKey}`;
 
-async function fetchGeoData() {
-    try {
-        const response = await fetch(url);
+try {
+    const response = await fetch(url);
+    if (response.ok) {
         const data = await response.json();
-
-        if (data.status.code === 200 && data.results.length > 0) {
-            const result = data.results[0];
-            const geoData = {
-                name: city,
-                long: result.geometry.lng,
-                lat: result.geometry.lat,
-                // The API may not provide ICAO, state, abbreviation, or elevation data
-                icao: 'N/A',
-                state: 'N/A',
-                abbr: 'N/A',
-                elv: 'N/A'
-            };
-            console.log([geoData]);
-        } else {
-            console.error('No data found for the specified location');
-        }
-    } catch (error) {
-        console.error('Error:', error);
+        console.log('Current weather data:', data);
+        // Extract and log specific wind data
+        console.log(`Wind Speed: ${data.wind.speed} m/s`);
+        console.log(`Wind Direction: ${data.wind.deg} degrees`);
+    } else {
+        console.error('Error fetching weather data:', response.statusText);
     }
+} catch (error) {
+    console.error('Error:', error);
 }
-
-fetchGeoData();
